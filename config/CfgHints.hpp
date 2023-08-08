72f33dcd024babaf413297fbaf2bb35be97cbe0a
hypoxia125 <amartini@martinicreations.com>
2023-08-08 19:22:41 -0400

    update

--- config/CfgHints.hpp
+++ config/CfgHints.hpp
@@ -7,7 +7,7 @@ class CfgHints
         {
             displayName = "Fortify Tool";
             displayNameShort = "Fortify Tool";
-            description = "Use the ACE Self Interact key to use the fortify tool in your inventory. Select an available fortification and follow the on screen prompts for placement";
+            description = "Use the ACE Self Interact key to use the fortify tool in your inventory. Select an available fortification and follow the on screen prompts for placement.";
             tip = "";
             arguments[] = {};
             image = "z\ace\addons\fortify\ui\hammer_ca.paa";
